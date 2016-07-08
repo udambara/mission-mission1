bcc6cc28504983e77bacb6a4e56c362fd5be0ad0
udambara <krivnon@gmail.com>
2016-07-08 18:51:15 +0900

    mission1

---
+++ app/src/main/java/com/example/android/ex1_emotion/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.android.ex1_emotion;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.PaintDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView Tvemotion;
+    Button btnhappy;
+    Button btngloomy;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    void init()
+    {
+        Tvemotion = (TextView)findViewById(R.id.tv_emotion);
+        btnhappy = (Button)findViewById(R.id.btn_happy);
+        btngloomy = (Button)findViewById(R.id.btn_gloomy);
+    }
+    public void onClickHappy(View view){
+        String emotion = btnhappy.getText().toString();
+        int color = ((ColorDrawable)btnhappy.getBackground()).getColor();
+        Tvemotion.setText(emotion);
+        Tvemotion.setTextColor(color);
+    }
+    public void onClickGloomy(View view){
+        String emotion = btngloomy.getText().toString();
+        int color = ((ColorDrawable)btngloomy.getBackground()).getColor();
+        Tvemotion.setText(emotion);
+        Tvemotion.setTextColor(color);
+    }
+}
